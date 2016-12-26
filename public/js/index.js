5e77717d9027ef9ec361f11d11f6ecd62218a4c8
arasmarko <marko@arsfutura.co>
2016-12-26 15:21:02 +0100

    mustache

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,24 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
 	var formattedTime = moment(message.creaatedAt).format('h:mm a');
-	console.log('new message: ', message);
-	var li = $('<li></li>');
-	li.text(`${formattedTime} ${message.from}: ${message.text}`);
-
-	$('#messages').append(li);
+	var template = $('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		time: formattedTime,
+		user: message.from
+	});
+	$('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
 	var formattedTime = moment(message.creaatedAt).format('h:mm a');
-	var li = $('<li></li>');
-	var a = $('<a target="_blank">My current location</a>');
-	li.text(`${formattedTime} ${message.from}: `);
-	a.attr('href', message.url);
-	li.append(a);
-	$('#messages').append(li);
+	var template = $('#location-message-template').html();
+	var html = Mustache.render(template, {
+		url: message.url,
+		time: formattedTime,
+		user: message.from
+	});
+	$('#messages').append(html);
 });
 
 
